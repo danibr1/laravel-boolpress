c57313f4c23e454d3c6c914dc99834ba1980a064
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-06-16 18:36:27 +0200

    Popolate posts

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -11,6 +13,14 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 0; $i < 5; $i++) {
+            $new_post = new Post();
+
+            $new_post->title = 'Post title' . ($i+1);
+            $new_post->slug = Str::slug($new_post->title,'-');
+            $new_post->content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore veniam nobis magnam dicta minima fugit animi autem aliquid ab debitis doloremque quaerat non sit a sint beatae porro, rerum ipsa!';
+
+            $new_post->save();
+        }
     }
 }
