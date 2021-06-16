89deccc963815a4f94c63384f1fc52da6a597739
danibr1 <75913820+danibr1@users.noreply.github.com>
2021-06-16 18:26:01 +0200

    set auth

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guest.welcome');
+});
+
+Auth::routes();
+
+// ROTTE AREA ADMIN
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->name('admin.')
+    ->group(function() {
+        // Rotta home Admin
+        Route::get('/', 'HomeController@index')->name('home');
+        // Rotte resource posts
+
+    });
